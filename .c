ff8702f48241e156fc1a5ef0abff1037ef2d885d
luizsantos7 <lhsantos098@gmail.com>
2024-09-02 14:01:27 -0300

    Locale h

--- .c
+++ .c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <locale.h>
 
 char nome[30];
-
+ 
 int main() {
 
-printf("Digite seu Nome: \n");
-scanf("%s", nome);
-printf("Seu nome eh %s", nome);
+ printf("Digite seu Nome: \n");
+ scanf("%s", nome);
+ printf("Seu nome é %s", nome);
+
+return 0;
 }
